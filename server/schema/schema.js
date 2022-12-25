3045a83a3becdb9a6b808c0ce262fa91623a895b
Yugesh Palvai <yugeshpalvai21@gmail.com>
2022-12-25 09:52:48 +0530

    adds and configures postgres client to node and used in graphql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,9 +1,63 @@
 const { authors, books } = require('../sampleData');
-
-console.log(authors);
-console.log(books);
-
 const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql');
+
+const { Client } = require('pg');
+
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'yugeshpalvai',
+  password: 'samplepassword',
+  database: 'bookmanagementapp'
+});
+
+// authors_create_query = `
+//   CREATE TABLE authors  (
+//   id serial PRIMARY KEY,
+//   name VARCHAR(255),
+//   location VARCHAR(255)
+//   )
+// `;
+
+// books_create_query = `
+//     CREATE TABLE books (
+//       id serial PRIMARY KEY,
+//       title VARCHAR(255),
+//       genre VARCHAR(255),
+//       author_id INTEGER REFERENCES authors(id)
+//     );
+// `;
+
+
+// add_default_authors_query = `
+//   INSERT INTO authors (name, location) 
+//   VALUES ('author1', 'location1'),
+//   ('author2', 'location2'),
+//   ('author3', 'location3');
+// `;
+
+// add_default_books_query = `
+//   INSERT INTO books (title, genre, author_id) 
+//   VALUES ('book1', 'genre1', 1),
+//   ('book2', 'genre2', 2),
+//   ('book3', 'genre3', 3),
+//   ('book4', 'genre4', 1),
+//   ('book5', 'genre1', 2);
+// `;
+
+client.connect((err) => {
+  if (err) {
+    console.error('connection error', err.stack)
+  } else {
+    console.log('connected')
+  }
+});
+
+// client.query(add_default_books_query, (err, res) => {
+//   if (err) throw err;
+//   console.log('ADDED DEFAULT BOOKS SUCCESSFULLY',res);
+//   client.end();
+// });
 
 const AuthorType = new GraphQLObjectType({
   name: 'Author',
@@ -11,12 +65,12 @@ const AuthorType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     location: { type: GraphQLString },
-    books: { 
-      type: new GraphQLList(BookType),
-      resolve(parent, args){
-        return books.filter(book => book.authorId === parent.id)
-       }
-    }
+    // books: { 
+    //   type: new GraphQLList(BookType),
+    //   resolve(parent, args){
+    //     return books.filter(book => book.authorId === parent.id)
+    //    }
+    // }
   })
 });
 
@@ -40,8 +94,15 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     authors: {
       type: new GraphQLList(AuthorType),
-      resolve(parent, args){
-        return authors;
+      resolve: (parent, args, context, info) => {
+        return new Promise((resolve, reject) => {
+          const query = `SELECT * FROM authors`; 
+          client.query(query, (err, res) => {
+            if (err) reject(err);
+            console.log(res.rows);
+            resolve(res.rows); 
+          });
+        });
       }
     },
     author: {
