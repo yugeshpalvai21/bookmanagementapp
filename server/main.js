5c6b18ca70ee9c6e61fcdbc007f1342f0e27fed0
Yugesh Palvai <yugeshpalvai21@gmail.com>
2023-01-26 13:58:14 +0530

    setup express server on port 3000 with rootpage

--- server/main.js
+++ server/main.js
@@ -1,1 +1,11 @@
-console.log("code running from main file");
+const express = require('express');
+
+const app = express();
+
+app.use("/", (request, response) => {
+  response.send("THIS IS ROOT PAGE");
+});
+
+app.listen(3000, () => {
+  console.log("SERVER IS RUNNING ON PORT 3000");
+});
