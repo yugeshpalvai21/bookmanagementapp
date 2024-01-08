9403ce46f88ee439979438d08cfa3605a38f7426
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-01-09 00:28:34 +0530

    installs ans sets up koa-static to serve static files

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,11 @@
 const Koa = require('koa');
+const path = require('path');
+const serve = require('koa-static');
 
 const app = new Koa();
 
-app.use(ctx => {
-    ctx.body = "Hello WORLD"
-});
+app.use(serve(path.join(__dirname, '..', 'public')));
 
 app.listen(3000, () => {
-    console.log("KOA SERVER STARTED AND LISTENING ON PORT 3000");
+    console.log("APP is running successfully on port 3000");
 });
