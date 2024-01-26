abcbec52ce9d21243ee9164a128893d325869f4f
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-01-26 22:40:25 +0530

    installs and configures dotenv

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,18 @@ const Koa = require('koa');
 const path = require('path');
 const serve = require('koa-static');
 
+// require and import local .env credentials
+const dotenv = require('dotenv');
+dotenv.config();
+
+// setup port based on environment
+const PORT = process.env.NODE_ENV == 'development' ? process.env.DEV_PORT : 8000;
+
+// configure app routes and run server
 const app = new Koa();
 
 app.use(serve(path.join(__dirname, '..', 'public')));
 
-app.listen(3000, () => {
-    console.log("APP is running successfully on port 3000");
+app.listen(PORT, () => {
+    console.log(`APP is running successfully on port ${PORT}`);
 });
